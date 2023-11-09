8d93ed31032beebed8727b5fabd4d58df692d90b
RSWilson1 <robert.s.wilson1996@gmail.com>
2023-11-09 10:52:36 +0000

    Adding datatables and creating base with nav-bar and extending.

--- vcfapp/views.py
+++ vcfapp/views.py
@@ -1,10 +1,24 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from pymongo import MongoClient
+import pandas as pd
+import retrying
+
+
+@retrying.retry(wait_fixed=1000, stop_max_delay=10000)
+def connect_to_mongodb():
+        try:
+            client = MongoClient('mongodb://localhost:27017', serverSelectionTimeoutMS=60000)
+            # Perform database operations here
+            return client
+        except Exception as e:
+            print(f"Failed to connect to MongoDB: {str(e)}")
+            raise
 
 def home(request):
-    # Connect to the MongoDB database
-    client = MongoClient('mongodb://localhost:27017/')
+    # Connect to MongoDB
+    client = connect_to_mongodb()
+    #client = MongoClient('mongodb://localhost:27017/')
     db = client['mydatabase']
     collection = db['variants']
 
@@ -12,9 +26,10 @@ def home(request):
     variants_list = list(collection.find())
 
     # Set up pagination
-    paginator = Paginator(variants_list, 50)  # Show 50 variants per page
+    paginator = Paginator(variants_list, 5000)  # Show 50 variants per page
     page_number = request.GET.get('page')
     variants = paginator.get_page(page_number)
 
     # Pass the paginated variants to the template
-    return render(request, 'home.html', {'variants': variants})
+    context = {'variants': variants}
+    return render(request, 'home.html', context)
